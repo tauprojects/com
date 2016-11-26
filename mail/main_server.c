01b25dfaeb987d66808b7f2241a81bee9928c5ab
Matan Gizunterman <Matan.Gizunterman@arm.com>
2016-11-26 18:06:12 +0200

    Mati

--- mail/main_server.c
+++ mail/main_server.c
@@ -17,35 +17,44 @@
 #include "mail_server.h"
 
 
-
-
 #define NUM_OF_USERS 1
 #define HELLO_MSG "‫!‪Welcome‬‬ ‫‪I‬‬ ‫‪am‬‬ ‫‪simple-mail-server.‬‬"
 
 
-//char** str_split(char* a_str, const char a_delim,int* countStr);
-
-int decInputState(char* client_message);
-
-//char** str_split(char* a_str, const char a_delim,int* countStr);
+//Functions Deceleration
+int getOpcode(char* client_message);
+int parseId(char* message);
 
 
+//Enum Message Operation Code Declarations
 typedef enum USER_COMMAND {
 	AUTH,
 	SHOW_INBOX,
 	GET_MAIL,
 	DELETE_MAIL,
-	QUIT,
-	COMPOSE
+	COMPOSE,
+	QUIT
+
 } USER_CMD;
 
 
 int listenSd; // The listen socket, defined as global for the code that exit with failure
 
 int main(int argc, char* argv[]) {
-	//Checking argument state
 	char *users_file;
 	int port;
+	int opCode;
+	int read_size;
+	int totalMailSize =MAX_SUBJECT + MAX_CONTENT + TOTAL_TO*MAX_USERNAME;
+	char client_message[totalMailSize];
+	char* username = (char*) malloc(sizeof(char) * MAX_USERNAME);
+	char* message;(char*) malloc(sizeof(char) * 2000);
+	USER user;
+	USER *users=(USER*)malloc(sizeof(USER)*NUM_OF_CLIENTS);
+
+
+
+	//Checking argument state
 	if (argc == 2) {
 		users_file = argv[1];
 		port = DEFAULT_PORT;
@@ -56,8 +65,18 @@ int main(int argc, char* argv[]) {
 		puts("invalid number of arguments");
 		return -1; //exit(1)
 	}
-//	UsersDB users = UsersDBCreate(users_file);
 
+
+
+	//Create Users DataBase
+	int sizeOfUsers = UsersDBCreate(users_file,users);
+	if(sizeOfUsers<0){
+		printf("Error While Parsing user.tx File");
+	}
+
+
+
+	//Listening Flow....
 	listenSd = socket(AF_INET, SOCK_STREAM, 0); // For type=SOCK_STREAM protocol 0 is TCP
 	if (listenSd == -1) {
 		printf("Failed to create the listen socket: %s\n", strerror(errno));
@@ -89,17 +108,86 @@ int main(int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	write(connSd, HELLO_MSG, strlen(HELLO_MSG));
-	int read_size;
-	int totalMailSize =MAX_SUBJECT + MAX_CONTENT + TOTAL_TO*MAX_USERNAME;
-	char client_message[totalMailSize];
-//	char* message = (char*)malloc(sizeof(char)*128);
+
 
 	//Receive a message from client
 	while ((read_size = recv(connSd, client_message, totalMailSize, 0)) > 0) {
-		//Send the message back to client
-//		parse_input = str_split((char*)client_message," ",&cnt);
-//		message = decInput(client_message);
-		write(connSd, "Mati", strlen("Connected to server‬‬"));
+
+		//Get Opcode of the first message in order to authenticate
+		opCode = getOpcode(client_message);
+
+		if(opCode!=AUTH){
+			printf("Error In Client Protocol Implementation - Server Expected Auth Message");
+			break;
+		}
+
+		//Handle Cases if user is authenticated
+		else if(auth_user(&client_message[1],username)){
+			user=  getUser(users,sizeOfUsers,username);
+			int id,ack;
+			while (1) {
+				switch (opCode) {
+
+				//Case 1 - Show Inbox Of the Authenticated Username
+				//client_message = "1\t"
+				case SHOW_INBOX:
+					//1
+					//Handle Case when no mails in DB!
+					ack = showInbox(user,message);
+					if(ack!=0){
+						printf("Error While Getting Inbox for Username: %s  - Internal Server Error",username);
+						break;
+					}
+					break;  //T0DO
+
+
+				//Case 2 - Get Mail Relative to ID number
+				//client_message = "2id\t" - Handle the Messeage
+				case GET_MAIL:
+						id = parseId(&client_message[1]);
+					    ack = getMail(user,id,message); //Parse mail ID
+						switch (ack) {
+						case -1:
+							printf("Error While Getting Mail ID Number: %d - Not Found in DB",id);
+							break;
+						case -2:
+							printf("Error While Getting Mail ID Number: %d  - Mail Marked as Trash",id);
+							break;
+						case -3:
+							printf("Error While Getting Mail ID Number: %d  - Internal Server Error",id);
+							break;
+						}
+						break;
+
+				//Case 3 - Delete Mail Relative to ID number
+				//client_message = "3id\t" - Handle the retuen Messeage
+				case DELETE_MAIL:
+					id = parseId(&client_message[1]);
+					ack = deleteMail(user, id); //T0DO
+					if(ack!=0){
+						printf("Error While Deleting Mail ID Number: %d - Not Found in DB",id);
+					}
+					break;
+
+				//Case 4 - Compose Mail
+				//client_message = "4content\0"
+				case COMPOSE:;  //Empty statement due to Unallowed declaration (stackoverflow)
+					MAIL mail = (MAIL) malloc(sizeof(mail));
+					char* unParsedMail = (char*) malloc(sizeof(char) * 1024);
+					strcat(unParsedMail, &client_message[1]);
+					parseMail(unParsedMail, username, mail);
+					printMail(mail);
+					break;
+
+				//Case 5 - Compose Mail
+				//client_message = "5\t"
+				case QUIT:
+					break;
+				printf("Error In Client Protocol Implementation-Server Expected Operation Message");
+				}
+			}
+		}
+		write(connSd, message, strlen("Connected to server‬‬"));
 	}
 
 	if (read_size == 0) {
@@ -113,47 +201,18 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
-
-int decInputState(char* client_message) {
+int getOpcode(char* client_message){
 	char* temp = (char*) malloc(sizeof(char) * 5);
 	strncpy(temp, client_message, 1);
 	int opCode = atoi(temp);
 	printf("\nstate is: %d", opCode);
-	char* username = (char*) malloc(sizeof(char) * MAX_USERNAME);
-
-	switch (opCode) {
-	case 0:
-//		0User: Mati    Password:correctS
-		printf("Authenticate!\n");
-		auth_user(&client_message[1],username); //T0DO
-		break;
-	case 1:
-		printf("Show Inbox!\n");
-//		showInbux(client_message); //T0DO
-	case 2:
-		printf("get mail\n");
-//		getMail(client_message); //T0DO
-		break;
-	case 3:
-		printf("delete mail\n");
-//		deleteMail(client_message); //T0DO
-		break;
-	case 4:
-		printf("\ncompose\n");
-		MAIL mail = (MAIL) malloc(sizeof(mail));
-		char* unpare  = (char*) malloc(sizeof(char) * 1024);
-		strcat(unpare,&client_message[1]);
-		parseMail(unpare,username,mail); //T0DO
-		printMail(mail);
-		break;
-	case 5:
-		printf("quit\n");
-		break;
-	default:
-		printf("quit\n");
-	}
 //	free(temp);
 	return opCode;
+}
+int parseId(char* message){
+	char* temp = strchr(message,'\t');
+	int id = atoi(message);
+	return id;
 }
 
 
